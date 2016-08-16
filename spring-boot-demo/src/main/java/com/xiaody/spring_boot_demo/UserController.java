d457b755110d8e4c210f6fa06d6ba9fe6044e27f
Xiaody <1307120838@qq.com>
2016-08-16 18:26:29 +0800

    fix spring-boot

--- spring-boot-demo/src/main/java/com/xiaody/spring_boot_demo/UserController.java
+++ spring-boot-demo/src/main/java/com/xiaody/spring_boot_demo/UserController.java
@@ -1,10 +1,10 @@
 package com.xiaody.spring_boot_demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 	
